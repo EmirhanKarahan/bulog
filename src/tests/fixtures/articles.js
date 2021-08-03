c3933fae8dfd6c7e84753a2b52dcd22748ef24f2
Emirhan KARAHAN <emirhan.karahan@outlook.com>
2021-08-03 14:32:32 +0300

    Add action test

---
+++ src/tests/fixtures/articles.js
@@ -0,0 +1,26 @@
+export default [
+  {
+    id: "1",
+    title: "First Article",
+    subtitle: "First Article's Subtitle",
+    content: "Cool article",
+    imageUrl: "https://firebasestorage.googleapis.com/1",
+    date: 0,
+  },
+  {
+    id: "2",
+    title: "Second Article",
+    subtitle: "Second Article's Subtitle",
+    content: "Really nice article",
+    imageUrl: "https://firebasestorage.googleapis.com/2",
+    date: 1000,
+  },
+  {
+    id: "3",
+    title: "Third Article",
+    subtitle: "Third Article's Subtitle",
+    content: "Superb article",
+    imageUrl: "https://firebasestorage.googleapis.com/3",
+    date: -1000,
+  },
+];

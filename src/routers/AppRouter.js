607e4ad8ac45bab0f8fdade127334047efff6e4c
Emirhan KARAHAN <emirhan.karahan@outlook.com>
2021-08-06 14:56:28 +0300

    Remove unnecessary imports&files

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Router, Route, Switch, Redirect} from "react-router-dom";
+import { Router, Route, Switch} from "react-router-dom";
 import { createBrowserHistory, createMemoryHistory } from "history";
 
 import Article from "../components/Article";

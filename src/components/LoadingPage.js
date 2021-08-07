e11ac0f7ae3e7c76a3fa047957b3551b479bb048
Emirhan KARAHAN <emirhan.karahan@outlook.com>
2021-08-07 16:39:44 +0300

    Add loading component

---
+++ src/components/LoadingPage.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function LoadingPage() {
+    return (
+        <div>
+            <img className="spinner" src="/images/spinner.svg" alt="spinner" />
+        </div>
+    )
+}

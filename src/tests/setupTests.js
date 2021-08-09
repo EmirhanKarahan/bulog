a61e0df9e1fc8a50344e8b5be4aa17a4dcd32700
Emirhan KARAHAN <emirhan.karahan@outlook.com>
2021-08-09 21:53:44 +0300

    Add enzyme, snapshot testing

--- src/tests/setupTests.js
+++ src/tests/setupTests.js
@@ -1,1 +1,6 @@
-require('dotenv').config({ path: '.env.test' });
+import {configure} from "enzyme";
+import Adapter from "@wojtekmaj/enzyme-adapter-react-17";
+
+configure({ adapter: new Adapter() });
+
+require("dotenv").config({ path: ".env.test" });
